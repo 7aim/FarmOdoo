3805e867a6aa76eb122fbf4031e88b71d67f7962
Aim <7asim6@gmail.com>
2025-09-04 11:59:36 +0400

    pull

--- models/farm_tractor_expense.py
+++ models/farm_tractor_expense.py
@@ -16,7 +16,7 @@ class FarmTractorExpense(models.Model):
     note = fields.Text('Qeyd')
     
     # Traktor məlumatları
-    tractor_name = fields.Char('Traktor Adı')
+    tractor_name = fields.Many2one('farm.tech', string='Texnika')
     expense_type = fields.Selection([
         ('fuel', 'Yanacaq'),
         ('repair', 'Təmir'),
