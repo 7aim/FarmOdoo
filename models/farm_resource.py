7f8ddef396f3d35e115548fbaa64f75247d0e59d
Aim <7asim6@gmail.com>
2025-09-30 22:31:00 +0400

    ---

--- models/farm_resource.py
+++ models/farm_resource.py
@@ -5,6 +5,17 @@ class FarmResource(models.Model):
     _description = 'Farm Resource'
 
     name = fields.Char(string='Vəsait Adı', required=True)
+    
+    # Vəsait növü
+    asset_type = fields.Selection([
+        ('machinery', 'Texnikalar'),
+        ('irrigation_equipment', 'Suvarma Avadanlıqları'),
+        ('buildings', 'Bina və Tikililər'),
+        ('movable_property', 'Daşınar Əmlaklar'),
+        ('perennial_plants', 'Çoxillik Bitkilər'),
+        ('other', 'Digər')
+    ], string='Vəsait Növü', required=True, default='machinery')
+    
     image = fields.Binary(string='Şəkil')
     note = fields.Text(string='Qeyd')
     area_id = fields.Many2one('farm.field', string='Sahə', required=True, ondelete='cascade')
