ffe46b5bdb98a0a6336729f7e9a9c09e0692f6f1
Aim <7asim6@gmail.com>
2025-10-26 15:02:27 +0000

    Test

--- models/farm_meter.py
+++ models/farm_meter.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from odoo import models, fields, api
 
 class FarmMeter(models.Model):
@@ -12,19 +11,4 @@ class FarmMeter(models.Model):
     
     installation_date = fields.Date(string='Quraşdırılma tarixi')
     location = fields.Char(string='Yerləşdiyi yer')
-=======
-from odoo import models, fields, api
-
-class FarmMeter(models.Model):
-    _name = 'farm.meter'
-    _description = 'Sayğac'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-
-    name = fields.Char(string='Sayğac adı', required=True)
-    serial_number = fields.Char(string='Seriya nömrəsi')
-    capacity_liters = fields.Float(string='Tutum (litr)')
-    
-    installation_date = fields.Date(string='Quraşdırılma tarixi')
-    location = fields.Char(string='Yerləşdiyi yer')
->>>>>>> a7a8fa33fd80e5758bb2299f93b01ef44ee824d5
     description = fields.Text(string='Açıqlama')
